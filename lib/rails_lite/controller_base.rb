d0155ee491acbb5a4cb1647f82c7257c5712b88b
enquea <student@appacademy.io>
2013-12-17 17:28:05 -0800

    Sessions functioning properly

--- lib/rails_lite/controller_base.rb
+++ lib/rails_lite/controller_base.rb
@@ -12,6 +12,7 @@ class ControllerBase
   end
 
   def session
+    @session ||= Session.new(@req)
   end
 
   def already_rendered?
@@ -20,21 +21,21 @@ class ControllerBase
   def redirect_to(url)
     @res.header["location"] = url
     @res.status = 302
+    @session.store_session(@res)
   end
-
 
   # started as render_content(content, type)
   def render_content(body, content_type)
     @res.content_type = content_type
     @res.body = body
     @already_built_response = true
+    session.store_session(@res)
   end
 
   def render(template_name)
     controller = self.class.to_s.underscore
     file_type = "html.erb"
     file_name = "views/#{controller}/#{template_name}.#{file_type}"
-    @name = "horatio"
     f = File.read(file_name)
     file_output = ERB.new(f).result(binding)
     render_content(file_output, 'html')
--- lib/rails_lite/session.rb
+++ lib/rails_lite/session.rb
@@ -2,15 +2,32 @@ require 'json'
 require 'webrick'
 
 class Session
+  attr_accessor :cookie
+
   def initialize(req)
+    @cookie = {}
+    req.cookies.each do |cookie|
+      puts "cookie name is #{cookie.name}"
+      puts "cookie value is #{cookie.value}"
+      if cookie.name == "_rails_lite_app"
+        @cookie = JSON.parse(cookie.value)
+      end
+    end
+    puts "-----------------"
+    puts "cookies are: #{req.cookies}"
+    puts "-----------------"
+
   end
 
   def [](key)
+    @cookie[key]
   end
 
   def []=(key, val)
+    @cookie[key] = val
   end
 
   def store_session(res)
+    res.cookies << WEBrick::Cookie.new("_rails_lite_app", @cookie.to_json)
   end
 end
